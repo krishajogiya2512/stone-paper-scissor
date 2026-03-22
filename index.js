d881d150dc31b2dff59dc6b0e66d917003cb039c
Krisha Jogiya <krishajogiya25@gmail.com>
2026-03-22 22:33:09 +0530

    updated UI and JS Logic

---
+++ index.js
@@ -0,0 +1,13 @@
+let msg = document.getElementById("msg");
+let historyBox = document.getElementById("history");
+
+let rockBtn = document.getElementById("rock");
+let paperBtn = document.getElementById("paper");
+let scissorBtn = document.getElementById("scissor");
+
+
+let playerScore = 0;
+let computerScore = 0;
+let roundCount = 0;
+
+let historyText = "";
